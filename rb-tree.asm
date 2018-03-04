e54ac15920c8feb3f012a96136f2a30b4213da67
Felipe Custódio <felipe.crochi@gmail.com>
2018-03-04 19:44:24 -0300

    Start working on assignment

---
+++ rb-tree.asm
@@ -0,0 +1,23 @@
+ # ,---- [ SSC-0112 - Organização de Computadores Digitais ]
+# |
+# |  Implementação de uma Árvore Rubro-Negra em Assembly MIPS
+# |  Alunos:
+# |        Felipe Scrochio Custódio - 9442688
+# |        Gabriel Henrique Scalici - 9292970
+# |        (...)
+# `----
+
+.text
+
+    main:
+
+
+
+
+
+    # Encerrar execução do programa
+    li $v0, 10
+    syscall
+
+
+.data
