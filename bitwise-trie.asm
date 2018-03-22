6efdbc9632b285814450810087c25dc24a09b67e
Felipe Custódio <felipe.crochi@gmail.com>
2018-03-22 09:42:53 -0300

    Initial template for assignment 1, references, menu

--- bitwise-trie.asm
+++ bitwise-trie.asm
@@ -1,131 +1,107 @@
- # ,---- [ SSC-0112 - Organização de Computadores Digitais ]
-# |
-# |  Implementação de uma Bitwise Trie em Assembly MIPS
-# |  Alunos:
-# |        Felipe Scrochio Custódio - 9442688
-# |        Gabriel Henrique Scalici - 9292970
-# |        (...)
-# `----
+#   SSC-0112 - Organização de Computadores Digitais
+#
+#   Implementação de uma Bitwise Trie em Assembly MIPS
+#
+#   Alunos:
+#       Felipe Scrochio Custódio - 9442688
+#       Gabriel Henrique Scalici - 9292970
+#       Juliano Fantozzi - ...
+#
+#   Recursos Utilizados:
+#   MIPS Green Sheet
+#   http://students.cs.tamu.edu/tanzir/csce350/reference/syscalls.html
+#   https://en.m.wikipedia.org/wiki/Trie
 
-# Recursos Utilizados:
-# MIPS Green Sheet
-# http://students.cs.tamu.edu/tanzir/csce350/reference/syscalls.html
-# https://en.m.wikipedia.org/wiki/Trie
+.data
 
-###### TODO ######
-# Coloquem dúvidas e coisas a fazer aqui
-# [] Usar .align
-# [] Alinhar e identar textos
+    # Strings de menu
+    str_menu: .asciiz "\n\nBITWISE TRIE\n\n    1. Inserção\n    2. Remoção\n    3. Busca\n    4. Visualização\n    5. Sair\n    Escolha uma opção (1 a 5): "
 
-#.data
+    # Strings das opções do menu
+    str_insert:     .asciiz     "Digite o binário para inserção: "
+    str_remove:     .asciiz     "Digite o binário para remoção: "
+    str_search:     .asciiz     "Digite o binário parra busca: "
 
-    ### data structures ###
-    # node:
-    # trie:
-
-    # Exemplo array:
-    # list: .word 3, 0, 1, 2, 6, -2, 4, 7, 3, 7
-
-    #### strings ####
-    # Visualizing the trie
-    # * Node
-    # +----+
-    # | %d |
-    # +----+
-    # %d = node's value
-    node_box_top:    .asciiz    "+----+\n| "
-    node_box_bottom: .asciiz    "  |\n+----+\n"
-
-    str_newline:     .asciiz     "\n"
-
-    # Menu
-    str_menu:       .asciiz     "\n\nBITWISE TRIE\n\n    1. Insert node\n    2. Search node\n    3. Delete node\n    4. Visualize trie\n    5. Exit\n\nSelect your option: \n"
-
-    str_op:         .asciiz     "Select option: "
-    str_insert:     .asciiz     "Value to insert: "
-    str_remove:     .asciiz     "Value to remove: "
-    str_search:     .asciiz     "Value to search: "
-    str_vis: .asciiz "Visualizing trie\n"
-    str_exit: .asciiz "Exiting...\n"
+    # Strings da visualização
+    str_vis_n: .asciiz "N"
+    str_vis_p1: .asciiz "("
+    str_vis_p2: .asciiz ")"
+    srt_vis_info_t: .asciiz "T"
+    str_vis_info_nt: .asciiz "NT"
+    str_vis_null: .asciiz "null"
 
 .text
 
-    # MAIN
     main:
 
-        # load menu options
-        li $s0, 1 # 1 - Insert
-        li $s1, 2 # 2 - Search
-        li $s2, 3 # 3 - Remove
-        li $s3, 4 # 4 - Visualize
-        li $s4, 5 # 5 - Exit
+        # Opções do Menu ficam armazenadas nos registradores $sX
+        li $s0, 1 # 1 - Inserção
+        li $s1, 2 # 2 - Remoção
+        li $s2, 3 # 3 - Busca
+        li $s3, 4 # 4 - Visualizar
+        li $s4, 5 # 5 - Sair
 
+        # Funcionalidade de Menu
         menu:
-            li $v0, 4 # print string
-            la $a0, str_menu #  menu
+
+            li $v0, 4 # imprimir string
+            la $a0, str_menu
             syscall
 
-            li $v0, 5 # read int
+            li $v0, 5 # ler inteiro
             syscall
-            move $t0, $v0 # store user input
+            move $t0, $v0 # guardar input em $t0
 
-            # check user choice
-            # TODO:
-            # Mudar p/ labels de menu_1, menu_2...
-            # Usar JAL em vez de BEQ?
-            # Funções da trie viram procedures
-            # com retorno $ra
             beq $t0, $s0, insert_node
-            beq $t0, $s1, search_node
-            beq $t0, $s2, delete_node
+            beq $t0, $s1, delete_node
+            beq $t0, $s2, search_node
             beq $t0, $s3, print_trie
             beq $t0, $s4, exit
             j menu
 
-    ### TRIE FUNCTIONS ###
+    # Funcionalidades da Trie
     insert_node:
-        li $v0, 4 # print string
+        li $v0, 4 # imprimir string
         la $a0, str_insert
 
-        li $v0, 5 # read int
+        li $v0, 5 # ler inteiro
         syscall
-        move $t0, $v0 # store user input
+        move $t0, $v0 # guardar input em $t0
 
-        syscall
         j menu
 
     delete_node:
-        li $v0, 4 # print string
+        li $v0, 4 # imprimir string
         la $a0, str_remove
 
-        li $v0, 5 # read int
+        li $v0, 5 # ler inteiro
         syscall
-        move $t0, $v0 # store user input
+        move $t0, $v0 # guardar input em $t0
 
-        syscall
         j menu
 
     search_node:
-        li $v0, 4 # print string
+        li $v0, 4 # imprimir string
         la $a0, str_search
         syscall
 
-        li $v0, 5 # read int
+        li $v0, 5 # ler inteiro
         syscall
-        move $t0, $v0 # store user input
+        move $t0, $v0 # guardar input em $t0
 
         j menu
 
     print_trie:
-        li $v0, 4 # print string
+        li $v0, 4 # imprimir string
         la $a0, str_vis
         syscall
+
         j menu
 
     exit:
-        li $v0, 4 # print string
+        li $v0, 4 # imprimir string
         la $a0, str_exit
         syscall
 
-        li $v0, 10
+        li $v0, 10 # finalizar execução
         syscall
