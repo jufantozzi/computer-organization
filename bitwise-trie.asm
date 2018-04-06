17540736115eece5becf119e17d467139b3473cc
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-04-06 16:24:56 -0300

    Cleanup, insertion algorithm only
    Make merges easy again

--- bitwise-trie.asm
+++ bitwise-trie.asm
@@ -83,9 +83,9 @@
 		# (TODO) Armazenar endereço que está em $v0 em root (store word)
 		move $s5, $v0
 
-
-
-	# Funcionalidade do Menu
+	# +------+
+	# | MENU |
+	# +------+
 	menu:
 
 		li $v0, 4 # imprimir string
@@ -105,6 +105,10 @@
 		j menu # loop (opção inválida)
 
 	# Funcionalidades da Trie
+
+	# +----------+
+	# | INSERÇÃO |
+	# +----------+
 	insert_node:
 		li $v0, 4 # imprimir string
 		la $a0, str_insert
@@ -163,7 +167,7 @@
 
 		insert_node_right:
 			# verificar se já existe (TODO)
-			# se não for null, continuar descendo
+			# se não for null, continuar descendo (TODO)
 			addi $a1, $a1, 1 # ir para próximo caractere na chave
 			j insert_node_loop
 
@@ -182,124 +186,14 @@
 			move $t1, 4($t1) # acessar novo nó direito
 			j insert_node_loop # retorna ao loop de inserção
 
-	remove_node:
-		li $v0, 4 # imprimir string
-		la $a0, str_remove
-		syscall
-
-		li $v0, 8 # ler string
-		la $a0, chave # armazenar input do usuário em 'chave'
-		li $a1, 64 # preparar para ler 64 bytes (16 dígitos)
-		syscall
-
-		jal check_input # verificar que input é válido (volta ao menu se -1)
-
-		j menu
-
-	search_node:
-		li $v0, 4 # imprimir string
-		la $a0, str_search
-		syscall
-
-		li $v0, 8 # ler string
-		la $a0, chave # armazenar input do usuário em 'chave'
-		li $a1, 64 # preparar para ler 64 bytes (16 dígitos)
-		syscall
-
-		jal check_input # verificar que input é válido (volta ao menu se -1)
-
-		j menu
-
-		search_node_left:
-			# armazenar nó atual na pilha (caminho)
-			# verificar próximo caractere da string
-			addi $a0, $a0, 4 # ir p/ endereço do próximo caractere
-			lb $t0, 0($a0)
-
-		search_node_right:
-			# armazenar nó atual na pilha (caminho)
-
-			# ok, esse nó existe?
-			# estamos em 0($a0), certo? logo, byte em 8($a0) nos dará sua existencia
-
-			# verificar próximo caractere da string
-			addi $a0, $a0, 4 # ir p/ endereço do próximo caractere
-			lb $t0, 0($a0)
-
-
-
 	# +--------------+
 	# | VISUALIZAÇÃO |
 	# +--------------+
-	print_trie:
-		# li $v0, 4 # imprimir string
-		# la $a0, str_vis
-		# syscall
 
-		j menu
 
 	# +--------------+
 	# | CHECAR INPUT |
 	# +--------------+
-	check_input:
-		# Percorrer string de entrada
-		li $t1, 48 # 0 em ASCII
-		li $t2, 49 # 1 em ASCII
-		li $t3, 45 # - em ASCII
-		li $t4, 10 # \n em ASCII
-		la $a1, chave # carregar endereço de chave em $a1
 
-		check_input_loop:
-			# Carregar valor do endereço em $t0
-			lb $t0, 0($a1)
-			# Verificar se bit atual é 0, 1
-			beq $t0, $t1, check_input_continue # checa se é 0
-			beq $t0, $t2, check_input_continue # checa se é 1
-			beq $t0, $t3, check_input_return # checa se é -
-			beq $t0, $t4, check_input_pass # verifica se é '\n', se chegou no fim
-			# não é 0, 1 ou -1
-			j check_input_error
-
-		# é 0 ou 1, continua
-		check_input_continue:
-			# Ver se está na posição final da entrada
-			lb $t0, 0($a1) # carrega byte sem sinal
-			beq $t0, $zero, check_input_pass # verifica se é '\0', se chegou no final, sucesso
-
-			addi $a1, $a1, 1 # Andar para o próximo char checando novamente se é válido
-			j check_input_loop
-
-		# voltar ao menu (-1)
-		check_input_return:
-			addi $a1, $a1, 1 # Andar para o próximo byte
-			lb $t0, 0($a1) # Carregar byte
-			# bne $t0, $t2, check_input_error # Checa se é 1
-			# Exibir string de retorno
-			li $v0, 4 # Imprimir string
-			la $a0, str_return
-			syscall
-			# voltar ao menu
-			j menu
-
-		check_input_error:
-			# exibir string de chave inválida
-			li $v0, 4 # imprimir string
-			la $a0, str_invalid
-			syscall
-
-			li $v0, -1 # -1 no retorno = erro
-			jr $ra # retornar
-
-		check_input_pass:
-			li $v0, 1 # 1 no retorno = sucesso
-			jr $ra # retornar
-
-	exit:
-		li $v0, 4 # imprimir string
-		la $a0, str_exit
-		syscall
-
-		li $v0, 10 # finalizar execução
-		syscall
 
 # FIM DO PROGRAMA
