d69e9a762396d2897290d87f08f24a5c470c396f
andrestorino <37665572+andrestorino@users.noreply.github.com>
2018-04-06 13:48:23 -0300

    Update bitwise-trie.asm
    * Agora os dois ponteiros da raiz são inicializados como nulo
    * Agora o check_input só retorna ao menu quando for exatamente "-1"
    * Retirei algumas linhas da busca que pareciam desnecessarias, tendo em vista que o check_input já garante que
    não terão caracteres diferentes de '0', '1', '\n' ou '\0'

--- bitwise-trie.asm
+++ bitwise-trie.asm
@@ -73,6 +73,12 @@
 		li $v0, 9 # alocar memória
 		la $a0, 8 # 1 nó = 8 bytes (2 endereços/ponteiros)
 		syscall
+		
+		# Colocando o valor null nos ponteiros do primeiro noh
+		# endereco a esquerda = null
+		sw $zero, 0($v0)
+		# endereco a direita = null
+		sw $zero, 4($v0)
 
 		# Armazenar endereço inicial da Trie
 		move $s5, $v0
@@ -109,6 +115,7 @@
 		syscall
 		# checar se chave é valida
 		jal check_input
+		# tentando inserir novamente caso chave invalida
 		bne $v0, 1, insert_node
 		# checar se chave já existe
 		jal search_node
@@ -128,7 +135,7 @@
 
 		li $v0, 8 # ler string
 		la $a0, chave
-		li $a1, 64
+		li $a1, 16
 		syscall
 
 		jal check_input
@@ -136,6 +143,7 @@
 		j menu
 
 	search_node:
+		# valores setados nos registradores durante o check_input
 		#t1 = '0'
 		#t2 = '1'
 		#t4 = '\n' 
@@ -144,11 +152,10 @@
 		lb $t0, 0($a1) #carrega primeiro digito do input
 		
 		search_node_loop:
-			beq $t0, $t4, node_found #fim da leitura
-			beq $t0, $t1, case_zero #caso byte == '0'
-			beq $t0, $t2, case_one #caso byte == '1'
-			addi $a1, $a1, 1 #movendo string
-			lw $t1, 4($s5) #carrega endereço "1" da arvore
+			beq $t0, $t4, node_found #fim da leitura '\n'
+			beq $t0, $zero, node_found #fim da leitura '\0'
+			beq $t0, $t1, case_zero #caso byte == '0' goto case_zero
+			beq $t0, $t2, case_one #caso byte == '1' goto case_one
 			j search_node_loop
 					
 		case_zero:
@@ -197,9 +204,10 @@
 			# Verificar se bit atual é 0, 1
 			beq $t0, $t1, check_input_continue # checa se é 0
 			beq $t0, $t2, check_input_continue # checa se é 1
-			beq $t0, $t3, check_input_return # checa se é -
+			beq $t0, $t3, check_input_return1 # checa se é -
 			beq $t0, $t4, check_input_pass # verifica se é '\n', se chegou no fim
-			# não é 0, 1 ou -1
+			beq $t0, $zero, check_input_pass # verifica se eh '\0'
+			# não é 0, 1, - ou final de string
 			j check_input_error
 
 		# é 0 ou 1, continua
@@ -207,10 +215,25 @@
 			# Ver se está na posição final da entrada
 			lb $t0, 1($a1) # carrega proximo byte da string
 			addi $a1, $a1, 1 # Andar para o próximo char
-			j check_input_loop #reinicia check_input_looooOOoop
+			j check_input_loop #reinicia check_input_loop
 
 		# voltar ao menu (-1)
-		check_input_return:
+		# checando se o byte seguido do '-' equivale ao digito '1'
+		check_input_return1:
+			lb $t0 1($a1)
+			beq $t0, $t2, check_input_return2
+			j check_input_error
+			
+		# checando se a string acabou apos o "-1"
+		check_input_return2:
+			lb $t0 2($a1)
+			# checando se eh '\n'
+			beq $t0, $t4, check_input_return3
+			# checando se eh '\0'
+			beq $t0, $zero, check_input_return 3
+			j check_input_error
+			
+		check_input_return3:
 			# Exibir string de retorno
 			# Imprimir string
 			li $v0, 4 
